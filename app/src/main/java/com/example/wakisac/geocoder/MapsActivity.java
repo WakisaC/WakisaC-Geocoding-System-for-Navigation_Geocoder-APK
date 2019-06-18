71b7f41c51f8a8be953412754b1fc0da0c174aba
unknown <wakisag@gmail.com>
2019-06-17 18:23:05 -0700

    final_get direction

--- app/src/main/java/com/example/wakisac/geocoder/MapsActivity.java
+++ app/src/main/java/com/example/wakisac/geocoder/MapsActivity.java
@@ -1,8 +1,12 @@
 package com.example.wakisac.geocoder;
 
-
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
@@ -24,15 +28,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     String getAddress;
-    String url ="http://192.168.137.1/getData2.php";
+    String url = "http://192.168.137.1/getData2.php";
 
+    public static final int LOCATION_REQUEST = 500;
     private GoogleMap mMap;
+    ArrayList markerPoints= new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,28 +64,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        getDirection(googleMap);
         //receive values from address activity
         Intent i = getIntent();
-        getAddress= i.getStringExtra("address");
+        getAddress = i.getStringExtra("address");
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
 
                     @Override
                     public void onResponse(String response) {
-                        try{
+                        try {
                             JSONObject obj = new JSONObject(response);
 
                             double lati = obj.getDouble("Latitude");
                             double longi = obj.getDouble("Longitude");
                             // Add a marker and move the camera
-                            LatLng location = new LatLng( lati, longi);
+                            LatLng location = new LatLng(lati, longi);
                             mMap.addMarker(new MarkerOptions().position(location).title(getAddress));
                             mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
                             mMap.setBuildingsEnabled(true);
 
-                        }catch (Exception e){
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
 
@@ -89,7 +96,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Toast.makeText(MapsActivity.this, "Connection Error!!", Toast.LENGTH_SHORT).show();
 
             }
-        }){
+        }) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
@@ -100,7 +107,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Mysingleton.getmInstance(MapsActivity.this).addToRequestque(stringRequest);
     }
 
-    public void goToLocation(){
+    public void goToLocation() {
         // get the latitude and longitude
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, null,
                 new Response.Listener<JSONObject>() {
@@ -111,12 +118,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             double longi = response.getDouble("Longitude");
 
                             // Add a marker and move the camera
-                            LatLng location = new LatLng( lat, longi);
+                            LatLng location = new LatLng(lat, longi);
                             mMap.addMarker(new MarkerOptions().position(location).title(getAddress));
                             mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
 
 
-                        }catch (JSONException e){
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
 
@@ -143,7 +150,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Toast.makeText(MapsActivity.this, "Connection Error!!", Toast.LENGTH_SHORT).show();
 
             }
-        }){
+        }) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
@@ -154,4 +161,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Mysingleton.getmInstance(MapsActivity.this).addToRequestque(stringRequest);
     }
 
+    public void getDirection(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQUEST);
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case LOCATION_REQUEST:
+                if (grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    mMap.setMyLocationEnabled(true);
+                }
+                break;
+        }
+    }
 }
